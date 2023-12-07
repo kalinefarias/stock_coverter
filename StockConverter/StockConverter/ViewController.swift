9ccb41bd905517223223597ea8e2630cfa568a59
Kaline <kalinemattos.danca@gmail.com>
2023-12-07 18:41:16 +0100

    Arquivos iniciais  X-code (#1)
    Co-authored-by: Kaline Farias <kalinefarias@Kalines-Mac-mini.local>

---
+++ StockConverter/StockConverter/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  StockConverter
+//
+//  Created by Kaline Farias on 7/12/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
